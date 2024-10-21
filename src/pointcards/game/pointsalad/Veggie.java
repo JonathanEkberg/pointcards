e090685b3621871a35534b2cc804bc0a6ec57a98
Jonathan Ekberg <jodrim@gmail.com>
2024-10-21 16:12:19 +0200

    Add veggie docs

--- src/pointcards/game/pointsalad/Veggie.java
+++ src/pointcards/game/pointsalad/Veggie.java
@@ -1,5 +1,36 @@
 package pointcards.game.pointsalad;
 
+/**
+ * The {@code Veggie} enum represents the different types of vegetables
+ * available in the Point Salad game. Each vegetable type can be used in
+ * various game mechanics, such as scoring criteria and card drafting.
+ * 
+ * <p>
+ * The available vegetable types are:
+ * </p>
+ * <ul>
+ * <li>{@link #CARROT}</li>
+ * <li>{@link #LETTUCE}</li>
+ * <li>{@link #TOMATO}</li>
+ * <li>{@link #ONION}</li>
+ * <li>{@link #PEPPER}</li>
+ * <li>{@link #CABBAGE}</li>
+ * </ul>
+ * 
+ * <p>
+ * Example usage:
+ * </p>
+ * 
+ * <pre>
+ * {@code
+ * Veggie veggie = Veggie.CARROT;
+ * System.out.println("Selected vegetable: " + veggie);
+ * }
+ * </pre>
+ * 
+ * @see pointcards.game.pointsalad.Card
+ * @see pointcards.game.pointsalad.criteria.ICriteria
+ */
 public enum Veggie {
     CARROT,
     LETTUCE,
