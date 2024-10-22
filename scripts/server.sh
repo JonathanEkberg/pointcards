c21213d2deaf7f7f541b344559f452269fd4bde9
Jonathan Ekberg <jodrim@gmail.com>
2024-10-22 16:48:47 +0200

    Update server script

--- scripts/server.sh
+++ scripts/server.sh
@@ -2,4 +2,4 @@
 # ./scripts/compile.sh
 
 # java -ea -cp "lib/json.jar:bin" pointcards.Main pointcards.Main --type server --manifest-path ./PSManifestV1.json --players 2 --bots 0 
-LOGGER_LEVEL=info java -ea -cp "lib/json.jar:bin" pointcards.Main pointcards.Main --type server --manifest-path ./PSManifestV1.json --players 2 --bots 0
+LOGGER_LEVEL=info java -ea -cp "lib/json.jar:bin" pointcards.Main pointcards.Main --type server --manifest-path ./PSManifestV1.json
